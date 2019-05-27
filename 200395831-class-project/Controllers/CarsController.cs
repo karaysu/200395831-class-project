9469b069d08cbafa055bc819e97f2353569b4210
karaysu <deepraman90@gmail.com>
2019-05-27 16:12:07 -0400

    Added MVC 0.1

---
+++ 200395831-class-project/Controllers/CarsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _200395831_class_project.Controllers
+{
+    public class CarsController : Controller
+    {
+        // GET: Cars
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 200395831-class-project/Models/Cars.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _200395831_class_project.Models
+{
+    public class Cars
+    {
+        public int ID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Type { get; set; }
+        public int Year { get; set; }
+
+    }
+}
